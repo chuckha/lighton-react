603421820ff43c81795126f71906ea2f52c9c588
cha <cha@etsy.com>
2015-04-25 00:18:38 -0400

    Initial commit

---
+++ app/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = React.createClass({
+	render() {
+		return (
+			<div className="app">
+				<p>Hello world from app</p>
+			</div>
+		);
+	}
+});
+
+export {App};
